d65ab9cf4c50c66af5670576e49bbe823cc221b7
akigupta131 <akigupta131@gmail.com>
2017-06-16 10:18:13 +0530

    Initialize map function written for multiple markers

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -54,4 +54,55 @@ angular.module('rajneethiApp')
 			}
 		})
 	}
+
+	function initializeMap(map_data) {
+		var map;
+		var bounds = new google.maps.LatLngBounds();
+		var mapOptions = {
+			mapTypeId: 'roadmap'
+		};
+						
+		// Display a map on the page
+		map = new google.maps.Map(document.getElementById("default"), mapOptions);
+		map.setTilt(45);
+			
+		// Display multiple markers on a map
+		var infoWindow = new google.maps.InfoWindow(), marker, i;
+		
+		// Loop through our array of markers & place each one on the map  
+		for( i = 0; i < map_data.length; i++ ) {
+
+			var booth_name = map_data[i]['booth_name']
+			var caste_percentage = map_data[i]['caste_percentage']
+			var lat = map_data[i]['latitude']
+			var long = map_data[i]['longitude']
+
+			map_data[i].content = "<h5>" + booth_name + '</h5><br>' + caste_percentage
+
+			var position = new google.maps.LatLng(lat, long);
+			bounds.extend(position);
+			marker = new google.maps.Marker({
+				position: position,
+				map: map,
+				title: booth_name
+			});
+			
+			// Allow each marker to have an info window    
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+					infoWindow.setContent(map_data[i].content);
+					infoWindow.open(map, marker);
+				}
+			})(marker, i));
+
+			// Automatically center the map fitting all markers on the screen
+			map.fitBounds(bounds);
+		}
+
+		// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+		var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+			this.setZoom(5);
+			google.maps.event.removeListener(boundsListener);
+		});
+	}
 }])
