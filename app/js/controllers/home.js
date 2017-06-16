1c034a4423c9bff099ef46c18f8ee6024ca2026d
akigupta131 <akigupta131@gmail.com>
2017-06-16 10:17:32 +0530

    Map data formed using the consituencyMap booths data

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -23,6 +23,32 @@ angular.module('rajneethiApp')
 			console.log(response);
 			if(response){
 				$scope.booths = response;
+				if (response.length) {
+				 	getAllCastes(data);
+					var map_data = [];
+					var latlong = [];
+					showPieChart(data[0]);
+					for (var i = 0; i < data.length; i++) {
+						latlong[i] = data[i].local_booth_panchayat_name;
+						$scope.booths.push(data[i].booth_panchayat_name);
+						// try {
+							if (latlong[i].charAt(0) == '@') {
+								var latlong_new = latlong[i].split('@')[1].split(',');
+							}
+							map_data[i] = {
+								latitude: Number(latlong_new[0]),
+								longitude: Number(latlong_new[1]),
+								booth_name: data[i].booth_panchayat_name,
+								caste_percentage: data[i].caste_equation_percentage
+							}
+					}
+					console.log('map_data')
+					console.log(map_data)
+					initializeMap(map_data);
+				} else {
+					initializeMap();
+					Materialize.toast('No booths in the project', 2000);
+				}
 			} else{
 				Materialize.toast(response.message, 2000);
 			}
